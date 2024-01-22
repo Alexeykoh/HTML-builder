ce6b2addc1aa0b4f7fd2fb037103e2de919557b4
Alexeykoh <alexeykoh@gmail.com>
2024-01-22 23:00:52 +0300

    fix: create ignoring folders

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 //
-const filePath = path.join(__dirname + '/secret-folder');
+const filePath = path.join(__dirname, '/secret-folder');
 //
 function readFiles() {
-  fs.readdir(filePath, (err, files) => {
+  fs.readdir(filePath, { withFileTypes: true }, (err, files) => {
     if (err) console.log(err);
-    else {
-      files.forEach(async (file) => {
-        const extname = path.extname(file).slice(1);
-        const basename = path.basename(file, extname).slice(0, -1);
-        const size = fs.statSync(path.join(filePath, file)).size;
+    //
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const folder = path.join(filePath, file.name);
+        const extname = path.extname(folder).slice(1);
+        const basename = path.basename(folder, extname).slice(0, -1);
+        const size = fs.statSync(folder).size;
         console.log(basename, ' - ', extname, ' - ', size);
-      });
-    }
+      }
+    });
   });
 }
 
