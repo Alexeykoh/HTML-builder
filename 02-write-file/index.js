d7cf65f9de03d5da52dd9ed9bd90c34a9467f699
Alexeykoh <Alexeykoh@icloud.com>
2024-01-23 16:06:11 +0300

    fix: create promise file creation

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,26 +3,32 @@ const path = require('path');
 const readline = require('readline');
 
 const filePath = path.join(__dirname, 'text.txt');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+fs.promises
+  .appendFile(filePath, '', function (err) {
+    if (err) throw err;
+  })
+  .then(() => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-rl.setPrompt('Enter your text: ');
-rl.prompt();
+    rl.setPrompt('Enter your text: ');
+    rl.prompt();
 
-rl.on('line', (input) => {
-  if (input === 'exit') {
-    console.log('Goodbye!');
-    process.exit(0);
-  }
-  fs.writeFile(filePath, input + '\n', { flag: 'a' }, function (error) {
-    if (error) return console.log(error);
+    rl.on('line', (input) => {
+      if (input === 'exit') {
+        console.log('Goodbye!');
+        process.exit(0);
+      }
+      fs.writeFile(filePath, input + '\n', { flag: 'a' }, function (error) {
+        if (error) return console.log(error);
+      });
+      rl.prompt();
+    });
+
+    rl.on('close', () => {
+      console.log('Goodbye!');
+      process.exit(0);
+    });
   });
-  rl.prompt();
-});
-
-rl.on('close', () => {
-  console.log('Goodbye!');
-  process.exit(0);
-});
